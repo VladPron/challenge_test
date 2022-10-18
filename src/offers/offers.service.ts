8fb42047cba405d614429104808f0e93305e8810
Vladislavpron0 <vladislavpron0@gmail.com>
2022-10-18 10:39:39 +0300

    fixed

--- src/offers/offers.service.ts
+++ src/offers/offers.service.ts
@@ -1,26 +1,35 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Offer } from '../entity/offer.entity';
-import { Repository } from 'typeorm';
+import { DataSource, Repository } from 'typeorm';
 import { payload } from '../../offer1.payload';
 import { payload as payloadOther } from '../../offer2.payload';
 import { OfferDto, ReadLoadPayloadDto } from './dto/payload.dto';
 import { CreateDto } from './dto/create.one.dto';
 import { OfferBoxSizeEnum } from 'src/enums/offerBoxSize.enum';
 import { ReadLoadPayloadOherDto } from './dto/payload.other.dto';
+import { DuplicateUniqueException } from 'src/errors/error.exception';
 
 @Injectable()
 export class OffersService {
   constructor(
     @InjectRepository(Offer)
     private readonly offerRepository: Repository<Offer>,
+    private readonly dataSource: DataSource,
   ) {}
 
   async createOffer() {
-    const data: ReadLoadPayloadDto = payload;
+    const queryRunner = this.dataSource.createQueryRunner();
 
-    return await Promise.all(
-      data.response.offers.map(async (offer: OfferDto) => {
+    const { response: offers }: ReadLoadPayloadDto = payload;
+    const newOffers = [];
+    try {
+      await queryRunner.connect();
+      await queryRunner.startTransaction();
+
+      for (const key in Object.keys(offers.offers)) {
+        const offer: OfferDto = offers.offers[key];
+
         const offerEntity: CreateDto = this.offerRepository.create();
         offerEntity.name = offer.offer_name;
         offerEntity.description = offer.offer_desc;
@@ -34,43 +43,64 @@ export class OffersService {
         offerEntity.offerUrlTemplate = offer.offer_url;
         offerEntity.providerName = offer.offer_name;
         offerEntity.externalOfferId = offer.offer_id;
-        try {
-          return await this.offerRepository.save(offerEntity);
-        } catch (error) {
-          return error.message;
-        }
-      }),
-    );
+
+        await queryRunner.manager.save(offerEntity);
+        newOffers.push(offerEntity);
+      }
+
+      await queryRunner.commitTransaction();
+    } catch (error) {
+      await queryRunner.rollbackTransaction();
+      if (error.code == '23505') {
+        throw new DuplicateUniqueException();
+      }
+      throw error;
+    } finally {
+      await queryRunner.release();
+    }
+    return newOffers;
   }
 
   async createOfferOther() {
-    const { data }: ReadLoadPayloadOherDto = payloadOther;
+    const queryRunner = this.dataSource.createQueryRunner();
     const offers = [];
-    for (const key in data) {
-      if (Object.prototype.hasOwnProperty.call(data, key)) {
-        const { Offer, OS } = data[key];
+    try {
+      await queryRunner.connect();
+      await queryRunner.startTransaction();
+
+      const { data }: ReadLoadPayloadOherDto = payloadOther;
+
+      for (const key of Object.keys(data)) {
+        const { Offer: offer, OS: os } = data[key];
 
         const offerEntity: CreateDto = this.offerRepository.create();
-        offerEntity.name = Offer.name;
-        offerEntity.description = Offer.description;
-        offerEntity.slug = Offer.name.toLowerCase().split(' ').join('');
-        offerEntity.requirements = Offer.instructions;
+        offerEntity.name = offer.name;
+        offerEntity.description = offer.description;
+        offerEntity.slug = offer.name.toLowerCase().split(' ').join('');
+        offerEntity.requirements = offer.instructions;
         offerEntity.thumbnail = '';
-        offerEntity.isDesktop = OS.web ? 1 : 0;
-        offerEntity.isAndroid = OS.android ? 1 : 0;
-        offerEntity.isIos = OS.ios ? 1 : 0;
-        offerEntity.box_size = OfferBoxSizeEnum.large;
-        offerEntity.offerUrlTemplate = Offer.tracking_url;
-        offerEntity.providerName = Offer.name;
-        offerEntity.externalOfferId = Offer.campaign_id;
+        offerEntity.isDesktop = os.web ? 1 : 0;
+        offerEntity.isAndroid = os.android ? 1 : 0;
+        offerEntity.isIos = os.ios ? 1 : 0;
+        offerEntity.box_size = offer.large;
+        offerEntity.offerUrlTemplate = offer.tracking_url;
+        offerEntity.providerName = offer.name;
+        offerEntity.externalOfferId = offer.campaign_id;
 
-        try {
-          offers.push(await this.offerRepository.save(offerEntity));
-        } catch (error) {
-          console.log(error.message);
-        }
+        await queryRunner.manager.save(offerEntity);
+        offers.push(offerEntity);
       }
+      await queryRunner.commitTransaction();
+    } catch (error) {
+      await queryRunner.rollbackTransaction();
+      if (error.code == '23505') {
+        throw new DuplicateUniqueException();
+      }
+      throw error;
+    } finally {
+      await queryRunner.release();
     }
+
     return offers;
   }
 }
